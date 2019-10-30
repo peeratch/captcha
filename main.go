ee51e2d828f0fed1a31145992afc608518b83de8
pirat phatphak <pirat.p@pirats-MacBook-Pro.local>
2019-10-30 20:23:28 +0700

    not used map version

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type do func(l, o, r int) string
 
 var (
-	m = []string{"+", "-", "*"}
-	n = []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	m  = []string{"+", "-", "*"}
+	n  = []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	fs = []do{
+		func(l, o, r int) string { return fmt.Sprintf("%v %s %v", l, m[o], n[r]) },
+		func(l, o, r int) string { return fmt.Sprintf("%v %s %v", n[l], m[o], r) },
+	}
 )
 
 func main() {
-	fmt.Print(g(1, 1, 1, 1))
+	fmt.Printf("%s", g(0, 1, 1, 1))
 }
 
 func g(f, l, o, r int) string {
-	m := map[int]string{
-		0: fmt.Sprintf("%v %s %v", l, m[o], n[r]),
-		1: fmt.Sprintf("%v %s %v", n[l], m[o], r),
-	}
-	return m[f]
+	return fs[f](l, o, r)
 }
