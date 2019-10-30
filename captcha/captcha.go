018340f31022d74df0240e06396ee52de91173bd
pirat phatphak <pirat.p@pirats-MacBook-Pro.local>
2019-10-30 16:32:23 +0700

    without-if refactoring

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -1,19 +1,27 @@
 package captcha
 
 import (
-	"captcha/strcon"
-	"errors"
 	"fmt"
 )
 
 var (
-	ErrUnsupportMathOperator   = errors.New("unsupport math operator")
-	ErrUnsupportFormatOperator = errors.New("unsupport format operator")
-
 	mathOperator = map[int]string{
 		0: "+",
 		1: "-",
 		2: "*",
+	}
+
+	numberAsText = map[int]string{
+		0: "zero",
+		1: "one",
+		2: "two",
+		3: "three",
+		4: "four",
+		5: "five",
+		6: "six",
+		7: "seven",
+		8: "eight",
+		9: "nine",
 	}
 )
 
@@ -31,40 +39,20 @@ type Captcha struct {
 	Format       int
 }
 
-func NewCaptcha(format, left, mathOperator, right int) *Captcha {
-	return &Captcha{
+func NewCaptcha(format, left, mathOperator, right int) string {
+	c := Captcha{
 		right,
 		left,
 		mathOperator,
 		format,
 	}
+	return c.generateCaptcha()
 }
 
-func (c *Captcha) Validate() error {
-	if _, ok := mathOperator[c.MathOperator]; !ok {
-		return ErrUnsupportMathOperator
+func (c *Captcha) generateCaptcha() string {
+	m := map[int]string{
+		LeftFormatOperator:  fmt.Sprintf(Format, c.Left, mathOperator[c.MathOperator], numberAsText[c.Right]),
+		RightFormatOperator: fmt.Sprintf(Format, numberAsText[c.Left], mathOperator[c.MathOperator], c.Right),
 	}
-	if c.Format != LeftFormatOperator && c.Format != RightFormatOperator {
-		return ErrUnsupportFormatOperator
-	}
-	return nil
-}
-
-func (c *Captcha) GenerateCaptcha() string {
-	if c.Format == LeftFormatOperator {
-		return c.doLeft()
-	}
-	return c.doRight()
-}
-
-func (c *Captcha) mathOperatorSymbol() string {
-	return mathOperator[c.MathOperator]
-}
-
-func (c *Captcha) doLeft() string {
-	return fmt.Sprintf(Format, c.Left, c.mathOperatorSymbol(), strcon.Ntot(c.Right))
-}
-
-func (c *Captcha) doRight() string {
-	return fmt.Sprintf(Format, strcon.Ntot(c.Left), c.mathOperatorSymbol(), c.Right)
+	return m[c.Format]
 }
--- main.go
+++ main.go
@@ -3,13 +3,8 @@ package main
 import (
 	"captcha/captcha"
 	"fmt"
-	"log"
 )
 
 func main() {
-	c := captcha.NewCaptcha(0, 1, 1, 1)
-	if err := c.Validate(); err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Print(c.GenerateCaptcha())
+	fmt.Print(captcha.NewCaptcha(0, 1, 1, 1))
 }
